1cb6e7c3d5fa1c3b7a7bc8dd1606573fe3db60ce
Oleksii Fadieiev <alexfdv1891@gmail.com>
2023-12-16 19:49:08 +0200

    Lesson 75: pointers to members of shared object.

---
+++ Lesson_75_std_shared_object_fields_access/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+struct person {
+    string name;
+    size_t age;
+    person(string n, size_t a) : name(move(n)), age{a} {
+        cout << "CTOR " << name << '\n';
+    }
+    
+    ~person() {cout << "DTOR : " << name << '\n';}
+};
+
+int main(int argc, char **argv)
+{
+	shared_ptr<string> shared_name;
+    shared_ptr<size_t> shared_age;
+    
+    {
+        auto sperson (make_shared<person>("John Doe", 30));
+        shared_name = shared_ptr<string>(sperson, &sperson->name);
+        shared_age = shared_ptr<size_t>(sperson, &sperson->age);
+    }
+    
+    cout << "name : " << *shared_name
+         << "\nage : " << *shared_age << '\n';
+	return 0;
+}
